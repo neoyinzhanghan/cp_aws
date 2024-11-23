afe86b80d1132441da398267c6b1bd241a8bb0c6
Neo Yin <yinzhang@berkeley.edu>
2024-11-23 15:16:54 -0800

    svs slide info

--- svs_info.py
+++ svs_info.py
@@ -5,8 +5,10 @@ svs_path = "/media/hdd3/neo/viewer_sample_huong/390359.svs"
 wsi = openslide.OpenSlide(svs_path)
 
 # print all the levels of the slide
-print(wsi.level_count)
+print(f"Level count: {wsi.level_count}")
 
 # get the mpp of the slide
 mpp_x = float(wsi.properties[openslide.PROPERTY_NAME_MPP_X])
+mpp_y = float(wsi.properties[openslide.PROPERTY_NAME_MPP_Y])
 
+print(f"mpp_x: {mpp_x}, mpp_y: {mpp_y}")
