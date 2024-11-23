95f2f2c53cef56945b20e9cf189eead78d13abe2
Neo Yin <yinzhang@berkeley.edu>
2024-11-23 15:52:18 -0800

    dzsave h5 svs removing the 7 max level assumpiton

---
+++ np_info.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+fg_np_path = "/media/hdd3/neo/viewer_sample_huong/website/foreground_mask.npy"
+val_np_path = "/media/hdd3/neo/viewer_sample_huong/website/value_mask.npy"
+
+# Load the numpy arrays
+fg_np = np.load(fg_np_path)
+val_np = np.load(val_np_path)
+
+# print the shape of the numpy arrays
+print(f"Foreground mask shape: {fg_np.shape}")
+print(f"Value mask shape: {val_np.shape}")
