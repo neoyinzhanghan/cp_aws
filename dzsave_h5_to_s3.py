c509686f9ee8c357916918909623f895540a50c3
Neo Yin <yinzhang@berkeley.edu>
2024-11-23 15:20:29 -0800

    dzsave h5 sv

--- dzsave_h5_to_s3.py
+++ dzsave_h5_to_s3.py
@@ -166,10 +166,3 @@ end_time = time.time()
 print("H5 file and heatmap uploaded successfully.")
 print(f"Time taken: {end_time - start_time:.2f} seconds.")
 
-if __name__ == "__main__":
-    svs_path = "/media/hdd3/neo/viewer_sample_huong/390359.svs"
-    h5_path = "/media/hdd3/neo/S3_tmp_dir/test_slide_2.h5"
-
-    dzsave_h5(
-        svs_path, h5_path, tile_size=512, num_cpus=32, region_cropping_batch_size=256
-    )
