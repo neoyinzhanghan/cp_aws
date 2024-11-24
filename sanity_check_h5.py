2a519600c3dbd353e7704ab2cfcb0b8a7853398f
Neo Yin <yinzhang@berkeley.edu>
2024-11-24 11:47:12 -0800

    debug

--- sanity_check_h5.py
+++ sanity_check_h5.py
@@ -18,3 +18,20 @@ for key in slide_h5.keys():
 for key in heatmap_h5.keys():
     print(f"Heatmap h5 key: {key}, dimensions: {heatmap_h5[key].shape}")
 
+# print the level_0_height and level_0_width
+level_0_height = slide_h5["level_0_height"][()]
+level_0_width = slide_h5["level_0_width"][()]
+patch_size = slide_h5["patch_size"][()]
+
+print(
+    f"Slide Level 0 height: {level_0_height}, Level 0 width: {level_0_width}, Patch size: {patch_size}"
+)
+
+# do the same for the heatmap
+level_0_height = heatmap_h5["level_0_height"][()]
+level_0_width = heatmap_h5["level_0_width"][()]
+patch_size = heatmap_h5["patch_size"][()]
+
+print(
+    f"Heatmap Level 0 height: {level_0_height}, Heatmap Level 0 width: {level_0_width}, Heatmap Patch size: {patch_size}"
+)
