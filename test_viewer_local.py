ce8054ff824f4423dff458cc584b176a9f2de131
Neo Yin <yinzhang@berkeley.edu>
2024-11-24 14:10:05 -0800

    more verbose debug log

--- test_viewer_local.py
+++ test_viewer_local.py
@@ -5,7 +5,6 @@ from flask import Flask, jsonify, send_file, render_template_string
 import h5py
 from PIL import Image
 from flask_cors import CORS
-from dzsave_h5 import retrieve_tile_h5
 
 app = Flask(__name__)
 CORS(app)
@@ -81,6 +80,59 @@ HTML_TEMPLATE = """
 </body>
 </html>
 """
+
+
+def image_to_jpeg_string(image):
+    # Create an in-memory bytes buffer
+    buffer = io.BytesIO()
+    try:
+        # Save the image in JPEG format to the buffer
+        image.save(buffer, format="JPEG")
+        jpeg_string = buffer.getvalue()  # Get the byte data
+    finally:
+        buffer.close()  # Explicitly close the buffer to free memory
+
+    return jpeg_string
+
+
+def jpeg_string_to_image(jpeg_string):
+    # Create an in-memory bytes buffer from the byte string
+    buffer = io.BytesIO(jpeg_string)
+
+    # Open the image from the buffer and keep the buffer open
+    image = Image.open(buffer)
+
+    # Load the image data into memory so that it doesn't depend on the buffer anymore
+    image.load()
+
+    return image
+
+
+def encode_image_to_base64(jpeg_string):
+    return base64.b64encode(jpeg_string)
+
+
+def decode_image_from_base64(encoded_string):
+    return base64.b64decode(encoded_string)
+
+
+def retrieve_tile_h5(h5_path, level, row, col):
+    with h5py.File(h5_path, "r") as f:
+        try:
+            jpeg_string = f[str(level)][row, col]
+            jpeg_string = decode_image_from_base64(jpeg_string)
+            image = jpeg_string_to_image(jpeg_string)
+
+        except Exception as e:
+            print(
+                f"Error: {e} occurred while retrieving tile at level: {level}, row: {row}, col: {col} from {h5_path}"
+            )
+            jpeg_string = f[str(level)][row, col]
+            print(f"jpeg_string: {jpeg_string}")
+            jpeg_string = decode_image_from_base64(jpeg_string)
+            print(f"jpeg_string base 64 decoded: {jpeg_string}")
+            raise e
+        return image
 
 
 @app.route("/")
