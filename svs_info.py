ea256ea0f51815e752328318f8a9eeabcd01d126
Neo Yin <yinzhang@berkeley.edu>
2024-11-24 15:12:27 -0800

    dyadic reorganization

--- svs_info.py
+++ svs_info.py
@@ -8,12 +8,6 @@ wsi = openslide.OpenSlide(svs_path)
 # print all the levels of the slide
 print(f"Level count: {wsi.level_count}")
 
-# get the mpp of the slide
-mpp_x = float(wsi.properties[openslide.PROPERTY_NAME_MPP_X])
-mpp_y = float(wsi.properties[openslide.PROPERTY_NAME_MPP_Y])
-
-print(f"mpp_x: {mpp_x}, mpp_y: {mpp_y}")
-
 # get the level 0 dimensions
 level_0_dimensions = wsi.level_dimensions[0]
 
