6e1621380b2baee0aa1fb8700fc02ec271d7eef1
Neo Yin <yinzhang@berkeley.edu>
2024-11-23 16:21:52 -0800

    np bm script

--- benchmark_np.py
+++ benchmark_np.py
@@ -13,4 +13,13 @@ print(f"Time taken to load numpy array: {time.time() - start_time} seconds")
 # convert the numpy array to an image
 start_time = time.time()
 slide_img = Image.fromarray(slide_np)
+# make sure the image is in RGB mode
+slide_img = slide_img.convert("RGB")
 print(f"Time taken to convert numpy array to image: {time.time() - start_time} seconds")
+
+height, width = slide_img.size
+
+# downsample the image by a factor of 2 and record how long it takes
+start_time = time.time()
+slide_img_resized = slide_img.resize((width // 2, height // 2))
+print(f"Time taken to resize image: {time.time() - start_time} seconds")
