61b50303837e593989c5a7555e4691f7e861fed8
Neo Yin <yinzhang@berkeley.edu>
2024-12-07 23:06:05 -0800

    finding svs files to tile

--- pancreas/get_slide_names.py
+++ pancreas/get_slide_names.py
@@ -5,4 +5,15 @@ dir_path = "/media/ssd2/huong/meningioma_train_log/pancreas_new/uni/predict/attn
 # get all the files in the directory
 files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
-print(files)
+splitter1 = "_rainbow_heatmap_mask"
+splitter2 = "_coolwarm_heatmap_mask"
+
+processed_files = []
+
+for file in files:
+    file = file.split(splitter1)[0]
+    file = file.split(splitter2)[0]
+
+    processed_files.append(file)
+
+print(processed_files)
