2107fd4ad48dbeb1d6884bb7620f3913366f8eca
Neo Yin <yinzhang@berkeley.edu>
2024-12-08 21:27:09 -0800

    test dep

--- pancreas/test_slide_deployment.py
+++ pancreas/test_slide_deployment.py
@@ -4,5 +4,7 @@ slide_h5_path = "/home/dog/Documents/neo/cp-lab-wsi-upload/wsi-and-heatmaps/23.C
 
 # try to get the level_0_width and level_0_height
 with h5py.File(slide_h5_path, "r") as f:
-    level_0_width = f["level_0_width"][0]
-    level_0_height = f["level_0_height"][0]
+    level_0_width = f["level_0_width"][()]
+    level_0_height = f["level_0_height"][()]
+
+print(level_0_width, level_0_height)
