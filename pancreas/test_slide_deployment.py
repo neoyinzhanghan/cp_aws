21041d5a379770cc8b2b1c24777040a6588634b9
Neo Yin <yinzhang@berkeley.edu>
2024-12-08 21:28:07 -0800

    test dep

--- pancreas/test_slide_deployment.py
+++ pancreas/test_slide_deployment.py
@@ -1,6 +1,6 @@
 import h5py
 
-slide_h5_path = "/home/dog/Documents/neo/cp-lab-wsi-upload/wsi-and-heatmaps/23.CFNA.113 A1 H&E _171848.h5"
+slide_h5_path = "/home/dog/Documents/neo/cp-lab-wsi-upload/wsi-and-heatmaps/390359.h5"
 
 # try to get the level_0_width and level_0_height
 with h5py.File(slide_h5_path, "r") as f:
