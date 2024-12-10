9c844a226a9b3f41a55f6dbc40e575b1c56c4b14
Neo Yin <yinzhang@berkeley.edu>
2024-12-10 00:03:10 -0800

    blabla

---
+++ pancreas/add_a_column.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+old_df_path = (
+    "/home/dog/Documents/neo/cp-lab-wsi-upload/wsi-and-heatmaps/pancreas_metadata.csv"
+)
+
+new_df_path = "/home/dog/Documents/huong/analysis/visualization/website/pancreas"
+
+# open the df_path as a pandas DataFrame
+df = pd.read_csv(old_df_path)
+new_df = pd.read_csv(new_df_path)
+
+# iterate through each row in the df
+for idx, row in df.iterrows():
+    case_name = row["case_name"]
+
+    new_df_row = new_df[new_df["case_name"] == case_name]
+
+    if new_df_row.empty:
+        raise ValueError(f"No matching file found for {case_name}")
+
+    prediction = new_df_row["pred"].values[0]
+
+    print(prediction)
